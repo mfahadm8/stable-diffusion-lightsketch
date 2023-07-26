9dc6da1d8bb401688fb64f48ca6e41e041b215f5
Muhammad Fahad Mustafa <mfahadm8@gmail.com>
2023-07-27 02:30:35 +0500

    script to push to ecr

---
+++ ecr_push.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query "Account" --output text)
+AWS_REGION=$(aws configure get region)
+if [ -z "$AWS_REGION" ]; then
+    EC2_AVAIL_ZONE=`curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone`
+    AWS_REGION="`echo \"$EC2_AVAIL_ZONE\" | sed 's/[a-z]$//'`"
+fi
+docker build -t lightsketch .
+
+aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com
+
+docker tag lightsketch:latest $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/lightsketch:latest
+
+docker push 188775091215.dkr.ecr.us-east-1.amazonaws.com/lightsketch:latest
