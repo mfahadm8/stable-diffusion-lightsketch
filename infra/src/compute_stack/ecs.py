e996557f89e569dee4921a319da5fc3ecd82f387
Muhammad Fahad Mustafa <mfahadm8@gmail.com>
2023-07-29 16:36:56 +0500

    debugging

--- infra/src/compute_stack/ecs.py
+++ infra/src/compute_stack/ecs.py
@@ -169,7 +169,7 @@ class Ecs(Construct):
             key_name=self._config["compute"]["ecs"]["app"]["ec2_keypair"],
             user_data=user_data,
             security_group=ec2_security_group,
-
+            associate_public_ip_address=True
         )
 
         self.asg = autoscaling.AutoScalingGroup(
@@ -180,7 +180,6 @@ class Ecs(Construct):
             max_capacity=self._config["compute"]["ecs"]["app"]["maximum_containers"],
             vpc=self._vpc,
             vpc_subnets=ec2.SubnetSelection(subnet_type=ec2.SubnetType.PUBLIC, one_per_az=True),
-            associate_public_ip_address=True,
             spot_price="0.50",
             new_instances_protected_from_scale_in =False,
             launch_template=launch_template,
