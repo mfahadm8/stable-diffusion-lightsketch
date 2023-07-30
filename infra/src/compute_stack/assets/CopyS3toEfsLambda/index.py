85ad256fa7faea0503b451d7929d4673acb112d7
Muhammad Fahad Mustafa <mfahadm8@gmail.com>
2023-07-31 01:13:35 +0500

    s3 pipeline logic successful

--- infra/src/compute_stack/assets/CopyS3toEfsLambda/index.py
+++ infra/src/compute_stack/assets/CopyS3toEfsLambda/index.py
@@ -7,23 +7,27 @@ logger.setLevel(logging.INFO)
 
 s3 = boto3.client("s3")
 
-BUCKET_NAME = os.getenv("BUCKET_NAME")
 EFS_MOUNT = os.getenv("EFS_MOUNT")
-TEMP_DIR = os.getenv("TEMP_DIR")
+TEMP_DIR = os.getenv("TEMP_DIR","/tmp")
 
 
 def handler(events, context):
-    print(events)
-    # s3_file_path = "{your-s3-file-path}"
-    # local_temp_file = f"{TEMP_DIR}/file-name.zip"
-    # efs_file_path = f"{EFS_MOUNT}/custom-filename.zip"
+    logger.info(events)
+    for record in events["Records"]:
+        bucket_name=record["s3"]["bucket"]["name"]
+        s3_file_path=record["s3"]["object"]["key"]
+        local_temp_file = f"{TEMP_DIR}/{s3_file_path}"
+        efs_file_path = f"{EFS_MOUNT}/{s3_file_path}"
+        tmp_folder_path = os.path.dirname(local_temp_file)
+        os.makedirs(tmp_folder_path, exist_ok=True)
+        efs_folder_path = os.path.dirname(local_temp_file)
+        os.makedirs(efs_folder_path, exist_ok=True)
+        logger.info(s3_file_path)
+        with open(local_temp_file, "wb") as f:
+            s3.download_fileobj(bucket_name, s3_file_path, f)
 
-    # os.makedirs(TEMP_DIR, exist_ok=True)
-    # with open(local_temp_file, "wb") as f:
-    #     s3.download_fileobj(BUCKET_NAME, s3_file_path, f)
+        os.system(f"cp {local_temp_file} {efs_file_path}")
 
-    # os.system(f"cp {local_temp_file} {efs_file_path}")
-
-    # print(os.system(f"ls -la {EFS_MOUNT}"))
+        logger.info(os.system(f"ls -la {EFS_MOUNT}"))
 
     return {"statusCode": 200}
