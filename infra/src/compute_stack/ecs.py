377109ea977f50ef96d3f848d3f340106db8ab2d
Muhammad Fahad Mustafa <mfahadm8@gmail.com>
2023-08-03 19:15:17 +0500

    added trining ecr

--- infra/src/compute_stack/ecs.py
+++ infra/src/compute_stack/ecs.py
@@ -130,7 +130,9 @@ class Ecs(Construct):
                         "measurement": [
                             {"name": "memory_total", "rename": "nvidia_smi_memory_total", "unit": "Megabytes"},
                             {"name": "memory_used", "rename": "nvidia_smi_memory_used", "unit": "Megabytes"},
-                            {"name": "memory_free", "rename": "nvidia_smi_memory_free", "unit": "Megabytes"}
+                            {"name": "memory_free", "rename": "nvidia_smi_memory_free", "unit": "Megabytes"},
+                            {"name": "utilization_memory", "rename": "nvidia_smi_utilization_memory"},
+                            {"name": "utilization_gpu", "rename": "nvidia_smi_utilization_gpu"},
                         ],
                         "metrics_collection_interval": 60
                     }
