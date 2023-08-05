b77e4f3f423c4ec743d8473aed11ec6a44d37720
Muhammad Fahad Mustafa <mfahadm8@gmail.com>
2023-08-05 16:59:20 +0500

    bug fix

--- ecr_push.sh
+++ ecr_push.sh
@@ -5,10 +5,10 @@ if [ -z "$AWS_REGION" ]; then
     EC2_AVAIL_ZONE=`curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone`
     AWS_REGION="`echo \"$EC2_AVAIL_ZONE\" | sed 's/[a-z]$//'`"
 fi
-docker build -t lightsketch-api .
+docker build -t lightsketch-app .
 
 aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com
 
-docker tag lightsketch-api:latest $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/lightsketch:latest
+docker tag lightsketch-app:latest $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/lightsketch-app:latest
 
-docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/lightsketch:latest
+docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/lightsketch-app:latest
